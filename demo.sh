1f3ca2a4e5a3b711d855d76e58c6c8e8cd73fb15
leelaprasadn <ec2-user@ip-172-31-16-78.ec2.internal>
2024-03-25 08:46:11 +0000

    after changing the branch

--- demo.sh
+++ demo.sh
@@ -1,2 +1,13 @@
-hello good morning, this is my very first commit
-
+echo "hello good morning, this is my very first commit"
+a=10
+b=20
+c=30
+if [ $a -gt $b ]
+then
+echo “$a is greater than $b”
+elif [ $b -gt $c ]
+then
+echo “$b is greater than $c”
+else
+echo “$c is greater than $a”
+fi
